a37d70669d235f605369e24a61abdd2c136c2f3a
alexgriff <alexalexgriffith@gmail.com>
2018-01-21 14:39:30 -0500

    add frontend prod url for ws connection

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.web_socket_server_url = "wss://hidden-names-backend.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['http://localhost:3000','https://localhost:3000' ]
+  config.action_cable.allowed_request_origins = ['http://localhost:3000','https://localhost:3000', 'http://hidden-names.herokuapp.com','https://hidden-names.herokuapp.com']
 
   # Code is not reloaded between requests.
   config.cache_classes = true
